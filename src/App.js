0fd41877e8cf1264a4fae627ee59240a157d935a
xShiro2 <daryllejumalon16@gmail.com>
2023-02-13 08:57:04 +0800

    feat: add toast notification

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import { useState, useEffect } from 'react'
 import { getCurrentTime, getFutureTime } from './hooks/time'
 
 import addNotification from 'react-push-notification';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const initialTime = {
   time: "",
@@ -52,7 +54,7 @@ function App() {
   const handleRepeat = (repeat) => {
     let name
 
-    switch(repeat) {
+    switch (repeat) {
       case "Once":
         name = "once"
         break
@@ -71,7 +73,7 @@ function App() {
   const handleAction = (action) => {
     let name
 
-    switch(action) {
+    switch (action) {
       case "Drink water":
         name = "drink"
         break
@@ -81,7 +83,7 @@ function App() {
       default:
         name = "custom"
     }
-    
+
     setActionChecked(initialActionChecked)
     setChecked(setActionChecked, name)
     setAction(action)
@@ -90,7 +92,7 @@ function App() {
   const handleTime = (time, format) => {
     let name
 
-    switch(time) {
+    switch (time) {
       case "10":
         name = "min_10"
         break
@@ -98,9 +100,9 @@ function App() {
         name = "min_20"
         break
       case "1":
-        if (format === "hour") 
+        if (format === "hour")
           name = "hr_1"
-        else 
+        else
           name = "custom"
         break
       default:
@@ -125,75 +127,93 @@ function App() {
 
   const pushNotification = () => {
     addNotification({
-        title: action ,
-        native: true // when using native, your OS will handle theming.
+      title: action,
+      native: true // when using native, your OS will handle theming.
     })
   }
 
+  const pushToast = () => {
+    toast(action, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+      });
+  }
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (getCurrentTime() === futureTime) {
-        pushNotification()
+    document.title = 'Remind Me';
+  }, []);
 
-        if (repeat === "Repeat") {
-          setFutureTime(getFutureTime(time, format))
+  useEffect(() => {
+      const interval = setInterval(() => {
+        if (getCurrentTime() === futureTime) {
+          pushNotification()
+          pushToast()
+
+          if (repeat === "Repeat") {
+            setFutureTime(getFutureTime(time, format))
+          }
         }
-      }
-    }, 1000)
+      }, 1000)
 
-    return () => clearInterval(interval)
-  }, [futureTime, time, format, repeat, action])
+      return () => clearInterval(interval)
+    }, [futureTime, time, format, repeat, action])
 
   useEffect(() => {
-    if (time && format && repeat && action) {
-      setFutureTime(getFutureTime(time, format))
-    }
+      if (time && format && repeat && action) {
+        setFutureTime(getFutureTime(time, format))
+      }
   }, [time, format, repeat, action])
 
   return (
-    <div className="w-full min-h-screen bg-slate-900 flex justify-center align-center">
-      <div className="flex flex-col gap-y-3 text-white md:w-5/6 lg:w-2/5 w-full p-5  flex justify-center align-center">
-        <div className="flex flex-col gap-y-2">
-          <div className="flex flex-row justify-between">
-            <p className="font-medium text-3xl">Remind me to</p>
-            <button onClick={reset}>Reset</button>
+      <div className="w-full min-h-screen bg-slate-900 flex justify-center align-center">
+        <div className="flex flex-col gap-y-3 text-white md:w-5/6 lg:w-2/5 w-full p-5  flex justify-center align-center">
+          <div className="flex flex-col gap-y-2">
+            <ToastContainer />
+            <div className="flex flex-row justify-between">
+              <p className="font-medium text-3xl">Remind me to</p>
+              <button onClick={reset}>Reset</button>
+            </div>
+            <div className={`w-full rounded-md ${action && time && repeat ? "bg-rose-600" : "bg-gray-700"} text-white px-5 py-10 text-center font-bold text-2xl`}>
+              {action && time && repeat ?
+                action + (repeat === 'Once' ? ' in ' : ' every ') + time + " " + (parseInt(time) > 1 ? format + "s" : format)
+                : "Set time and action"
+              }
+            </div>
           </div>
-          <div className={`w-full rounded-md ${action && time && repeat ? "bg-rose-600" : "bg-gray-700"} text-white px-5 py-10 text-center font-bold text-2xl`}>
-            {action && time && repeat ?
-              action + (repeat === 'Once' ? ' in ' : ' every ') + time + " " + (parseInt(time) > 1 ? format + "s" : format)
-              : "Set time and action"
-            }
-          </div>
-        </div>
 
-        <div className="flex flex-col gap-y-2">
-          <p className="">Set Time</p>
-          <div className="flex flex-row gap-x-2">
-            <Time time="10" format={'minute'} onChange={handleTime} checked={timeChecked.min_10}/>
-            <Time time="20" format={'minute'} onChange={handleTime} checked={timeChecked.min_20}/>
-            <Time time="1" format={'hour'} onChange={handleTime} checked={timeChecked.hr_1}/>
-            <CustomTime setTime={handleTime} checked={timeChecked.custom}/>
+          <div className="flex flex-col gap-y-2">
+            <p className="">Set Time</p>
+            <div className="flex flex-row gap-x-2">
+              <Time time="10" format={'minute'} onChange={handleTime} checked={timeChecked.min_10} />
+              <Time time="20" format={'minute'} onChange={handleTime} checked={timeChecked.min_20} />
+              <Time time="1" format={'hour'} onChange={handleTime} checked={timeChecked.hr_1} />
+              <CustomTime setTime={handleTime} checked={timeChecked.custom} />
+            </div>
+            <p className="">Set Repeat</p>
+            <div className="flex flex-row gap-x-2">
+              <Repeat repeat={'Once'} onChange={handleRepeat} checked={repeatChecked.once} />
+              <Repeat repeat={'Repeat'} onChange={handleRepeat} checked={repeatChecked.repeat} />
+            </div>
           </div>
-          <p className="">Set Repeat</p>
-          <div className="flex flex-row gap-x-2">
-            <Repeat repeat={'Once'} onChange={handleRepeat} checked={repeatChecked.once}/>
-            <Repeat repeat={'Repeat'} onChange={handleRepeat} checked={repeatChecked.repeat}/>
-          </div>
-        </div>
 
-        <div className="flex flex-col gap-y-2">
-          <p className="">Set Action</p>
-          <div className="flex flex-row gap-2">
-            <Action action="Drink water" onChange={handleAction} checked={actionChecked.drink}/>
-            <Action action="Take a break" onChange={handleAction} checked={actionChecked.break}/>
-            <CustomAction setAction={handleAction} checked={actionChecked.custom}/>
+          <div className="flex flex-col gap-y-2">
+            <p className="">Set Action</p>
+            <div className="flex flex-row gap-2">
+              <Action action="Drink water" onChange={handleAction} checked={actionChecked.drink} />
+              <Action action="Take a break" onChange={handleAction} checked={actionChecked.break} />
+              <CustomAction setAction={handleAction} checked={actionChecked.custom} />
+            </div>
           </div>
-        </div>
 
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 
-export default App;
+  export default App;
